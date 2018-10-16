94d743c6f4157f335f898fdfa2a7bcb0ff708b89
Popov Timofei <P_timofei@mail.ru>
2018-10-16 21:48:52 +0300

    added greeting Git info

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int main() {
   cout << "Enter your name: ";
   cin >> name;
   cout << "Hello, " << name << "!\n";
+  cout << "Nice to see you learning Git\n";
   system("pause");
   return 0;
 }
+
 
